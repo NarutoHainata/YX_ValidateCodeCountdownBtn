bc21e58b0fc13851407406c433df322b025bfe8d
populary <410445919@qq.com>
2016-07-30 18:44:58 +0800

    Initial Commit

---
+++ YX_ValidateCodeCountdownBtn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YX_ValidateCodeCountdownBtn
+//
+//  Created by yang on 16/7/30.
+//  Copyright © 2016年 poplary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YX_ValidateCodeCountdownBtn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YX_ValidateCodeCountdownBtn
+//
+//  Created by yang on 16/7/30.
+//  Copyright © 2016年 poplary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
